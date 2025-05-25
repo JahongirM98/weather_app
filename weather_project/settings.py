95baf81ca91a2cfcaeb54718351cf67c8c9f317c
Jahongir Mirhalikov <3690969006@my.lerna.tj>
2025-05-25 16:52:30 +0500

    Add weather template, implement API integration and apply initial migrations

--- weather_project/settings.py
+++ weather_project/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +27,9 @@ SECRET_KEY = 'django-insecure-7yssevfh_w3=5kcpj&bn47%)oxij@p1oekk!!*v5gbn3swq+i9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '192.168.90.101'
+]
 
 
 # Application definition
@@ -55,7 +59,7 @@ ROOT_URLCONF = 'weather_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
