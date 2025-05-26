596512853ffd93f5151739b6d5868ef02032f99d
Jahongir Mirhalikov <3690969006@my.lerna.tj>
2025-05-26 17:26:37 +0500

    Add unit tests for main view: valid, invalid, and GET request

--- weather/tests.py
+++ weather/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
-# Create your tests here.
+class WeatherTests(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_index_loads(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_valid_city_forecast(self):
+        response = self.client.post('/', {'city': 'London'})
+        self.assertContains(response, 'Погода в London', status_code=200)
+
+    def test_invalid_city_shows_error(self):
+        response = self.client.post('/', {'city': 'Несуществующийгород123'})
+        self.assertContains(response, 'Город не найден', status_code=200)
